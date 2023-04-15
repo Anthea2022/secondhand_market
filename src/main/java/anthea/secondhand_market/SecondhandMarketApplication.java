61acdbd74c2972b75a42f0bd72b0b5ad7150748d
Anthea <striveandyou@163.com>
2023-04-15 12:38:28 +0800

    Initial commit

---
+++ src/main/java/anthea/secondhand_market/SecondhandMarketApplication.java
@@ -0,0 +1,13 @@
+package anthea.secondhand_market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondhandMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecondhandMarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/anthea/secondhand_market/SecondhandMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package anthea.secondhand_market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondhandMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
